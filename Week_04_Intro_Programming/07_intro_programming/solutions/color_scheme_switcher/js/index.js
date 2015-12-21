49bf751ab086e823f4ad4155812a99d9e8dbeae1
lolifigue <LoliFigue@Doloress-MacBook-Air.local>
2015-12-20 20:27:56 -0600

    Uploading Layout challenge: layouts 1 to 3
    Uploading Layout challenge: layouts 1 to 3

--- Week_04_Intro_Programming/07_intro_programming/solutions/color_scheme_switcher/js/index.js
+++ Week_04_Intro_Programming/07_intro_programming/solutions/color_scheme_switcher/js/index.js
@@ -1,12 +1,24 @@
 document.getElementById('grayButton').onclick = switchGray;
+
 document.getElementById('whiteButton').onclick = switchWhite;
 
+document.getElementById('blackButton').onmouseover = switchBlack;
+
 function switchGray() {
-  document.body.style.backgroundColor = 'gray';
- document.body.style.color = 'white';
+	document.body.style.backgroundColor = 'gray';
+	document.body.style.color = 'white';
 }
 
 function switchWhite() {
   document.body.style.backgroundColor = 'white';
   document.body.style.color = 'black';
+}
+
+function switchBlack() {
+	// alert("hello"); to check if it works
+	console.log("this is a console log");
+	console.log(document.body.style.backgroundColor);
+	document.body.style.backgroundColor = 'black';
+	console.log(document.body.style.backgroundColor);
+
 }
